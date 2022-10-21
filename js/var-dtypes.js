4651af2273504df8a3c4f88a2cf892e01ae03f00
Slady <csl.bunny.2207@gmail.com>
2022-10-21 23:56:47 +0200

    variables and data types

---
+++ js/var-dtypes.js
@@ -0,0 +1,24 @@
+var x = 3;
+window.console.log(x);
+
+var y;
+y = 5;
+var z;
+
+z = x + y;
+window.console.log("the type of z is " + typeof z);
+
+window.console.log("the value of z is " + z);
+var greeting = "Hello";
+var firstname = "Slady";
+var message = greeting + " " + firstname;
+window.console.log("the value of message is: " + message);
+window.console.log("the type od message is: " + typeof message);
+
+var check = x == 6;
+window.console.log("the value of check is: " + check);
+window.console.log("the tupe of check is: " + typeof check);
+
+var myArray = [1, 2, 3];
+window.console.log(myArray);
+window.console.log(typeof myArray);
