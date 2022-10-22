83907893c0e8190ba4f5ae6835714dcdbb2d4156
Slady <csl.bunny.2207@gmail.com>
2022-10-23 00:24:35 +0200

    fixed bug

--- js/functions.js
+++ js/functions.js
@@ -15,8 +15,8 @@ var x = sum(3, 7);
 // window.alert(x);
 
 function swap(id1, id2) {
-  var y = document.getElementById("id1").innerHTML;
-  var z = document.getElementById("id2").innerHTML;
+  var y = document.getElementById(id1).innerHTML;
+  var z = document.getElementById(id2).innerHTML;
 
   document.getElementById(id2).innerHTML = y;
   document.getElementById(id1).innerHTML = z;
