ba503149a2cace80b5fc2f0750dfe20bcfada19e
Slady <csl.bunny.2207@gmail.com>
2022-10-24 23:39:10 +0200

    if else

---
+++ js/if-switch.js
@@ -0,0 +1,8 @@
+var speed = 60;
+if (speed > 70) {
+  window.alert("You are going too fast!");
+} else if (speed < 40) {
+  window.alert("Your going too slow!");
+} else {
+  window.alert("Your speed is fine.");
+}
