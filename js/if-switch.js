ed3ed69797c50179f448514353408ffc483b9d34
Slady <csl.bunny.2207@gmail.com>
2022-10-24 23:45:31 +0200

    if else button check

--- js/if-switch.js
+++ js/if-switch.js
@@ -1,8 +1,19 @@
-var speed = 60;
-if (speed > 70) {
-  window.alert("You are going too fast!");
-} else if (speed < 40) {
-  window.alert("Your going too slow!");
-} else {
-  window.alert("Your speed is fine.");
+// var speed = 60;
+// if (speed > 70) {
+//   window.alert("You are going too fast!");
+// } else if (speed < 40) {
+//   window.alert("Your going too slow!");
+// } else {
+//   window.alert("Your speed is fine.");
+// }
+
+function checkSpeed() {
+  var speed = document.getElementById("speed").value;
+  if (speed > 70) {
+    window.alert("You are going too fast!");
+  } else if (speed < 40) {
+    window.alert("Your going too slow!");
+  } else {
+    window.alert("Your speed is fine.");
+  }
 }
